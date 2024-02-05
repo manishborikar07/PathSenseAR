b91565432d87f16cda1c54cdb36f0b5cd4425bc2
manishborikar07 <manishborikar07@gmail.com>
2024-02-05 22:16:47 +0530

    Destination Marker

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get HTML elements
     const destinationSelectInput = document.getElementById('select-destination');
     const destinationSelectButton = document.getElementById('get-direction-button');
-    const mapContainer = document.getElementById('map-container');
+    const mapContainer = document.getElementById('map');
     let map;
     let compass;
     // To keep track of the marker at the current location
