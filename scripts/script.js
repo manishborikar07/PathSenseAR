13e9d56e352ce1519a1dca9f7f281701c7853966
manishborikar07 <manishborikar07@gmail.com>
2024-02-08 20:22:39 +0530

    Set MultiFunction Button Working

--- scripts/script.js
+++ scripts/script.js
@@ -103,21 +103,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const recenterButton = document.getElementById('multifunction-button');
     recenterButton.addEventListener('click', () => {
         
-        if(!isMapCentered){
-        // Set the isUserInteraction flag to false after recentering
-        isUserInteraction = false;
-        isMapCentered = true; // Set the map-centered flag to true
-        }
-
-        // If the map is centered or bearing is not applied, start map bearing
-        if (isMapCentered || !isBearing) {
-            isBearing = true; // Set the bearing flag to true
-        }
-
         // If the map is centered, bearing is applied, and there's no destination, stop the map rotation
         if (isMapCentered && isBearing && !destination) {
             isBearing = false; // Set the bearing flag to false
             map.setBearing(0); // Stop the map rotation
+        }
+
+        // If the map is centered or bearing is not applied, start map bearing
+        if (isMapCentered) {
+            isBearing = true; // Set the bearing flag to true
+        }
+
+        // If I am not at center, set center
+        if(!isMapCentered){
+            // Set the isUserInteraction flag to false after recentering
+            isUserInteraction = false;
+            // Set the map-centered flag to true
+            isMapCentered = true; 
         }
 
         // If there's a destination, the map is centered, and bearing is applied, reset all
