cff7400a506fb2b3ea1e2e5a6f601373af1b351d
manishborikar07 <manishborikar07@gmail.com>
2024-03-17 00:22:05 +0530

    ar route

--- scripts/script.js
+++ scripts/script.js
@@ -356,7 +356,7 @@ document.addEventListener('DOMContentLoaded', function () {
         marker.setAttribute('depth', '1'); // Adjust marker depth based on distance
         marker.setAttribute('rotation', `0 ${rotation} 0`); // Rotate the marker
         marker.setAttribute('color', '#3882f6'); // Set the marker color
-        marker.setAttribute('opacity', '0.8'); // Set marker opacity
+        marker.setAttribute('opacity', '1'); // Set marker opacity
         // marker.setAttribute('scale', '1 1 1'); // Adjust scale as needed
         // marker.setAttribute('position', '0 0 0'); // Adjust position relative to camera
         
