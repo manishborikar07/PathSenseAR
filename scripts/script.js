758e0136ea63000174f16ffa1eeb315b6a18a012
manishborikar07 <manishborikar07@gmail.com>
2024-03-16 23:07:53 +0530

    ar route

--- scripts/script.js
+++ scripts/script.js
@@ -333,7 +333,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const angleDeg = (angleRad * 180) / Math.PI;
 
         // Return the rotation in format "x y z" (for example, "0 45 0" for a 45-degree rotation around the y-axis)
-        return angleDeg + 10;
+        return angleDeg + 7;
     };
 
     // Function to create a marker at a specified coordinate
